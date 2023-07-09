9b8c6c58ed53b2891e21e293645a1c9d00f6f28b
Popsicool <akinolasamson1234@gmail.com>
2023-07-09 22:45:07 +0100

    added cron jobs

---
+++ app/schedular_update.py
@@ -0,0 +1,31 @@
+import csv
+from datetime import date
+from django.conf import settings
+from django.core.mail import EmailMessage
+
+
+def update_something():
+    months = str(date.today()).split("-")[1]
+    day = str(date.today()).split("-")[2]
+    birthdays = []
+    with open("app/alumni.csv", "r") as file:
+        reader = csv.reader(file)
+        for row in reader:
+            mon = row[8].split("-")[0]
+            if mon == months and row[8].split("-")[1] == day:
+                obj = {}
+                obj["name"] = row[3]
+                obj["gender"] = row[6]
+                obj["email"] = row[1]
+                birthdays.append(obj)
+    if len(birthdays) > 0 :
+        celebrants = "Today's birthday celebrants are\n"
+        for celeb in birthdays:
+            celebrants += celeb['name'] + " "
+            text = f"Dear {celeb['name']},\n\nWe wish you a very happy birthday. Our prayer for you today is that the Lord grant you all your desires and answer all your prayers\nWe love you, and we celebrate you.\n\nCCCSP Funaab Alumni Body"
+            email_subject = f'Happy Birthday {celeb["name"]}'
+            email= EmailMessage(subject=email_subject,body=text, from_email= settings.EMAIL_FROM_USER,to=[celeb['email']])
+            email.send()
+        email_subject = 'Birthday Alert'
+        email= EmailMessage(subject=email_subject,body=celebrants, from_email= settings.EMAIL_FROM_USER,to=[settings.EMAIL_FROM_USER])
+        email.send()
--- csp/settings.py
+++ csp/settings.py
@@ -10,6 +10,8 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
+from decouple import config
+
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -122,3 +124,13 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+EMAIL_FROM_USER= config('EMAIL_FROM_USER')
+EMAIL_TO_USER= config('EMAIL_TO_USER')
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.gmail.com'
+SERVER_EMAIL = config('EMAIL_HOST_USER')
+EMAIL_HOST_USER = config('EMAIL_HOST_USER')
+EMAIL_HOST_PASSWORD = config('EMAIL_HOST_PASSWORD')
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
