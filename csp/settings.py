4edbe30613958ea764d3703d3fe3fee98703a5db
Popsicool <akinolasamson1234@gmail.com>
2023-07-09 16:29:13 +0100

    changed allowed host

--- csp/settings.py
+++ csp/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-1dhcmntwcc3)1obbff7fr7x*8_#!yti-g2je&k8y$j=emonu7*
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
